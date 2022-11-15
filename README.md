5afb6e803190c02ffb5a68529e1008af453b17cb
RKPurple <r.kallur.21@gmail.com>
2022-11-15 00:53:06 -0500

    random link gen

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Testing out Rust language interactions with Rasberry Pi's
 
 Learning how to use raspian os and git pull :(
+
+Tested open crate by opening a link in main funciton it worked.
+
+Created a random number generator using ascii values and sent to a imgur site randomly
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rand::Rng;
+fn make_ext() -> String {
+    let mut str = String::new();
+    for _ in 0..5 {
+        let random_letter: u8 = rand::thread_rng().gen_range(97..123); // generate a 8bit unsigned int that corresponds to a-z in ascii
+        let random_number: u8 = rand::thread_rng().gen_range(48..58); // generate a 8bit unsigned int that corresponds to 0-9 in ascii
+        let n = rand::thread_rng().gen_range(0..2); // pick 1 or 0
+        if n == 0 {
+            str.push(random_letter as char); // convert the ascii value to a character and add to the string
+        } else {
+            str.push(random_number as char); // convert the ascii value to a character and add to the string
+        }
+    }
+    return str;
+}
+
 fn main() {
-    open::that("https://www.youtube.com/watch?v=LDU_Txk06tM").unwrap();
+    let mut path = String::from("https://i.imgur.com/");
+    path.push_str(&make_ext());
+    open::that(&path).unwrap();
 }
